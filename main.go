72175204d03fd2e9aef65aefcff1761e3e0338f8
Gopal Thakur <send2gopal@gmail.com>
2023-09-28 00:37:57 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/send2gopal/gocrud/initializer"
+)
+
+func init() {
+	initializer.LoadEnvironmentvariables()
+}
+
+func main() {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
